369c1cc022935b6012357ced69d3fb6c45582391
Jane-QinJ <1139804380@qq.com>
2019-02-05 22:03:56 +0800

    自定义音乐播放项目， 用到try catch 部分知识

---
+++ HeadFirstJava/MidiSound/MiniMiniMusicApp.java
@@ -0,0 +1,40 @@
+import javax.sound.midi.*;
+public class MiniMiniMusicApp{
+    public static void main(String[] args){
+	MiniMiniMusicApp mini = new MiniMiniMusicApp();
+	mini.play();
+}//close main
+
+public void play(){
+    try{
+        Sequencer player = MidiSystem.getSequencer();
+        player.open();  //get a Sequencer and open it
+
+        Sequence seq = new Sequence(Sequence.PPQ, 4);
+
+        Track track = seq.createTrack(); //ask the sequence for a Track. The Track lives in the Sequence, and the MIDI data lives in the Track.
+        //Make a  Message
+        ShortMessage a = new ShortMessage();
+       ShortMessage first = new ShortMessage();
+        //Put the  Instruction  in the Message
+        a.setMessage(144, 1, 20, 100);
+       first.setMessage(192, 1, 102, 100);
+//Make a new  MidiEvent  using the Message
+        MidiEvent noteOn = new MidiEvent(a, 1);
+        MidiEvent noteOn2 = new MidiEvent(first,2);
+//Add the MidiEvent to the  Track
+        track.add(noteOn);
+      track.add(noteOn2);
+
+        ShortMessage b = new ShortMessage();
+        b.setMessage(128,1,44,100);
+        MidiEvent noteOff = new MidiEvent(b, 16);
+        track.add(noteOff);
+
+         player.setSequence(seq); //Give the Sequence to the Sequencer (like putting the CD in the CD player)
+         player.start(); //Start() the Sequencer (like pushing PLAY)
+}catch(Exception ex){
+        ex.printStackTrace();
+}
+}// close play
+} // close class
